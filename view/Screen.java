84d58625a961ccaa693fae4de59cfe0a23671f2c
Alexis TADIFO <alexis.tadifo@gmail.com>
2018-11-11 12:09:16 +0100

    Définition des entités à utiliser dans l'application

--- view/Screen.java
+++ view/Screen.java
@@ -1,1 +1,7 @@
-/* New file to manage Screen interfaces*/
+/** 
+* <h1>Screen</h1>
+* Interface des écrans. Chaque écran repréente une étape lors du processus de PM.
+* @author Alexis TADIFO
+* @version 0.1 
+* @since 2018/11/10 
+*/
